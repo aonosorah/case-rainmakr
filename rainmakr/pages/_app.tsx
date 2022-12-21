c0c36fc071a0c5070665f69eff584bd8764b8432
aonosorah <102835599+aonosorah@users.noreply.github.com>
2022-12-21 15:33:04 -0300

    solving bugs

--- rainmakr/pages/_app.tsx
+++ rainmakr/pages/_app.tsx
@@ -8,17 +8,17 @@ export default function App({ Component, pageProps }: AppProps) {
       <Global />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="icon" href="/favicon.ico"></link>
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
-        />
+        ></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
-        />
+        ></link>
       </Head>
       <Component {...pageProps} />
     </>
