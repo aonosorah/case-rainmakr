acefa8a4704db56a6c695cd31d86375bc9c52571
aonosorah <102835599+aonosorah@users.noreply.github.com>
2022-12-21 15:05:15 -0300

    menu added

--- rainmakr/components/header.tsx
+++ rainmakr/components/header.tsx
@@ -3,7 +3,13 @@ import Link from 'next/link';
 import React from 'react';
 import logo from '../assets/logo.svg';
 import menu from '../assets/menu.svg';
-import { HeaderBlock } from '../styles/headerstyle';
+import { HeaderBlock, DivMenu, Nav } from '../styles/headerstyle';
+import search from '../assets/magnifying.svg';
+import notify from '../assets/notify.svg';
+import jobs from '../assets/jobs.svg';
+import link from '../assets/link.svg';
+import group from '../assets/group.svg';
+import user from '../assets/user.svg';
 
 export default function Header() {
   return (
@@ -13,9 +19,51 @@ export default function Header() {
           <Image width="125.4" height="24" src={logo} alt="logo image" />
         </Link>
       </div>
-      <div>
-        <Image src={menu} alt="" />
-      </div>
+      <DivMenu>
+        <div className="div-img">
+          <Image src={menu} alt="" />
+        </div>
+        <Nav>
+          <ul>
+            <Link href="./user">
+              <li className="li-img">
+                <Image width={32} height={32} src={user} alt="user image" />
+                My Profile
+              </li>
+            </Link>
+            <Link href="./notifications">
+              <li>
+                <Image width={24} height={24} src={notify} alt="bell" />
+                Notifications
+              </li>
+            </Link>
+            <Link href="./jobs">
+              <li>
+                <Image width={24} height={24} src={jobs} alt="jobs" />
+                Rainjobs
+              </li>
+            </Link>
+            <Link href="">
+              <li>
+                <Image width={24} height={24} src={link} alt="links" />
+                Rainlinks
+              </li>
+            </Link>
+            <Link href="">
+              <li>
+                <Image width={24} height={24} src={group} alt="group" />
+                Groups
+              </li>
+            </Link>
+            <Link href="">
+              <li className="last-child">
+                <Image width={20} height={20} src={search} alt="search" />
+                Search
+              </li>
+            </Link>
+          </ul>
+        </Nav>
+      </DivMenu>
     </HeaderBlock>
   );
 }
--- rainmakr/styles/headerstyle.tsx
+++ rainmakr/styles/headerstyle.tsx
@@ -7,9 +7,70 @@ export const HeaderBlock = styled.div`
   z-index: 1;
   height: 40px;
   justify-content: space-between;
+  position: relative;
   align-items: center;
   padding: 0 17px;
   div {
     display: flex;
+  }
+`;
+export const DivMenu = styled.div`
+  position: absolute;
+  flex-direction: column;
+  align-items: flex-end;
+  width: 100%;
+  right: 0;
+  top: 0;
+  min-height: 40px;
+  justify-content: center;
+
+  .div-img {
+    display: flex;
+    align-items: center;
+    height: 40px;
+    padding-right: 17px;
+    background-color: transparent;
+  }
+  :hover {
+    nav {
+      display: block;
+    }
+  }
+`;
+export const Nav = styled.nav`
+  display: none;
+  background: #273539;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  width: 100%;
+  text-transform: uppercase;
+  ul {
+    list-style: none;
+    padding-left: 23px;
+    padding-right: 55px;
+    padding-top: 23px;
+    padding-bottom: 19px;
+    .last-child {
+      border-bottom: none !important;
+      gap: 22.5px;
+    }
+    .li-img {
+      gap: 12px;
+    }
+  }
+  a {
+    text-decoration: none;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 16px;
+    letter-spacing: 0.5px;
+    color: #12cce4;
+  }
+  li {
+    height: 40px;
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    border-bottom: 1px solid #42585f;
   }
 `;
